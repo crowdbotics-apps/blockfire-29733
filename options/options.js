46802bd2a120a1fc003239475336c0f48e3c92b5
Crowdbotics <team@crowdbotics.com>
2021-08-11 22:07:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "blockfire_29733Identifier",
+  url: "https://blockfire_29733Identifier.botics.co",
+  api: "https://blockfire_29733Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
